1d2f941d7dee999125111324779e3c16064edab2
MSpilari <mb.spilari@hotmail.com>
2025-02-13 11:38:30 -0300

    docs: add the anotations of swagger docs on find, delete and create user

--- src/main/java/com/testcontainers/examples/controllers/UserController.java
+++ src/main/java/com/testcontainers/examples/controllers/UserController.java
@@ -14,8 +14,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.testcontainers.examples.dto.CreateUserDto;
+import com.testcontainers.examples.dto.ErrorResponseDto;
 import com.testcontainers.examples.models.UserModel;
 import com.testcontainers.examples.services.UserService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping("/users")
@@ -33,11 +40,23 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Find user by id", description = "Return 200 if the user exists.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "User found", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "Wrong/not valid UUID", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponseDto.class))),
+            @ApiResponse(responseCode = "500", description = "default error", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponseDto.class)))
+    })
     public ResponseEntity<UserModel> getUserById(@PathVariable String id) {
         return ResponseEntity.ok().body(userService.findById(UUID.fromString(id)));
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete user by id")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "User deleted"),
+            @ApiResponse(responseCode = "400", description = "Wrong/not valid UUID", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponseDto.class))),
+            @ApiResponse(responseCode = "500", description = "default error", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponseDto.class)))
+    })
     public ResponseEntity<?> deleteUserById(@PathVariable String id) {
         userService.deleteById(UUID.fromString(id));
 
@@ -45,6 +64,8 @@ public class UserController {
     }
 
     @PostMapping("/")
+    @Operation(summary = "Create a new user", description = "Return 201 if the user was successfully created.")
+    @ApiResponse(responseCode = "201", description = "User created succesfully.")
     public ResponseEntity<UserModel> createUser(@RequestBody CreateUserDto userInfo) {
         var newUser = userService.save(userInfo);
 
