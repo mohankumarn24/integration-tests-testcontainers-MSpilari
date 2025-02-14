717002f7055cb1009e33fda43d099d759d7139cd
MSpilari <mb.spilari@hotmail.com>
2025-02-14 17:29:07 -0300

    feat: add pagination to the getAllUsers controller

--- src/main/java/com/testcontainers/examples/controllers/UserController.java
+++ src/main/java/com/testcontainers/examples/controllers/UserController.java
@@ -3,6 +3,10 @@ package com.testcontainers.examples.controllers;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.testcontainers.examples.dto.CreateUserDto;
@@ -35,8 +40,15 @@ public class UserController {
     }
 
     @GetMapping("/")
-    public ResponseEntity<List<UserModel>> getAllUsers() {
-        return ResponseEntity.ok().body(userService.allUsers());
+    public ResponseEntity<Page<UserModel>> getAllUsers(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id") String sortBy) {
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
+        Page<UserModel> users = userService.allUsers(pageable);
+
+        return ResponseEntity.ok().body(users);
     }
 
     @GetMapping("/{id}")
