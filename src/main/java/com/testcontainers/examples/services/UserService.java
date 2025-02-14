fb53cfb2d8fcb002eb9803339c511b4bf4581ac2
MSpilari <mb.spilari@hotmail.com>
2025-02-14 17:27:24 -0300

    feat: add pagination to AllUsers method

--- src/main/java/com/testcontainers/examples/services/UserService.java
+++ src/main/java/com/testcontainers/examples/services/UserService.java
@@ -3,6 +3,8 @@ package com.testcontainers.examples.services;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,8 +28,8 @@ public class UserService {
         return userRepository.save(newUser);
     }
 
-    public List<UserModel> allUsers() {
-        return userRepository.findAll();
+    public Page<UserModel> allUsers(Pageable pageable) {
+        return userRepository.findAll(pageable);
     }
 
     public UserModel findById(UUID id) {
